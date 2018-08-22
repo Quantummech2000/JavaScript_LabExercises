40d5dd93028fa470f371ac702f9a5b6ec30257ed
VVR(QuantumMechanic) <2.July.18@gmail.com>
2018-08-22 21:11:31 +0530

    Initial Push

---
+++ chessboard.js
@@ -0,0 +1,16 @@
+/** JS file containing the Chess board Pattern
+* 	-------------------------------------------------------------
+*   We try to define a 2D Array which reads the input and output 
+*   If the row+col sum is even print a * else print a White-Space
+*	With this logic we can reduce the complexity of the program.
+*	In order to make it universal -- portable to a matrix of size
+*  	n, I will read the size of the array and calculete the sum of
+*	the row and col index. After which we can decide if its a * 
+*  	or a White-Space.
+*	-------------------------------------------------------------
+*	Signed-off By: VishnuvardhanReddy (Quantummechanic2000)
+*/
+function chess_pattern(){
+		//WIP Comeback Later :D
+
+}
